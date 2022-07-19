b6d5573c357ef62686ed47a2b4b108c9987a3c73
장우주 <ozoo109@naver.com>
2022-07-19 17:53:36 +0900

    parking page 수정

--- front_main/src/components/Parking.js
+++ front_main/src/components/Parking.js
@@ -1,9 +1,22 @@
+import Box from "./Box";
+
+
 export default function Parking() {
     return (
-        <ul>
-            <li>
-                2번!
-            </li>
-        </ul>
+        <div>
+            <ul>
+                <li> / (빈공간/총대수)</li>
+            </ul>
+            <ui>
+                <Box name='첫번째'/>
+                <Box name='두번째'/>
+                <Box name='세번째'/>
+                <Box name='네번째'/>
+                <Box name='다섯번째'/>
+                <Box name='여섯번째'/>
+                <Box name='일곱번째'/>
+            </ui> <br />
+
+        </div>
     )
 };
