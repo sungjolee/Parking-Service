b871b5378d5947c799a475788d65bb840b52bac7
장우주 <ozoo109@naver.com>
2022-07-26 15:40:25 +0900

    사용안하는 jsx 삭제 및 정리

--- front_main/src/components/Parking/Box.jsx
+++ front_main/src/components/Parking/Box.jsx
@@ -2,22 +2,43 @@ import React from 'react';
 import styled from 'styled-components'
 
 const Box = styled.button`
-  border:1px solid red;
+  top : 30px;
+  border:2px solid white;
   width: 100px;
-  height: 100px;
-  transform: rotate(45deg);
+  height: 150px;
+  display: inline-block;
+  background-color: gray;
+`
+// transform: rotate(45deg); 주차장 기울이질 때 사용(숫자부분에 각도 넣기)
+
+const Myzone = styled.button`
+  background-color: white;
+  color: black;
+  border: 2px solid black;
 `
 
-
-
 export default function BoxClick() {
-  const clickMe = ()=>{
-    alert('이미 주차된 자리입니다.')
-  }
+  // const changeRed = () => {
+  //   return <Box>background-color: red</Box>
+  // }
   
+  // const alertParking = () => {
+  //   return alert ('이미 주차된 자리입니다.')
+  // }
+
+  // const clickMe = ()=>{
+  //   if (true) {
+  //     return changeRed } 
+  //   else { alertParking }}
+
+  const clickMe = ()=>{
+    alert ('이미 주차된 자리입니다.')
+  }
+
+
   return (
     <Box onClick={clickMe}> 
-      내 자리 표시하기
+      <Myzone>선택</Myzone>
     </Box>
   )
 };
