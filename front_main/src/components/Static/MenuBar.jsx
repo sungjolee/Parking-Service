7637544ab8ba14fc6892b890e2a03684591112ab
SSAFY <lsj2837@naver.com>
2022-08-10 11:23:34 +0900

    mui botton navigation 작업중

--- front_main/src/components/Static/MenuBar.jsx
+++ front_main/src/components/Static/MenuBar.jsx
@@ -1,6 +1,14 @@
-import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import styled from "styled-components";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import SearchIcon from '@mui/icons-material/Search';
+import HomeIcon from '@mui/icons-material/Home';
+import LocalParkingIcon from '@mui/icons-material/LocalParking';
 
 const Menubar = styled.div`
   position: fixed;
@@ -12,99 +20,42 @@ const Menubar = styled.div`
   justify-content: space-around;
   width: 280px;
   height: 50px;
-  border-radius: 30px;
+  border-radius: 50px;
   background-color: lightgray;
   position: fixed;
 `;
 
-const Home = styled.button`
-  height: 40px;
-  width: 80px;
-  background-color: #4f4587;
-  border-radius: 30px;
-  border: 0;
-  outline: 0;
-  margin: 3px;
-  a:link {
-    color: pink;
-  }
-  a:visited {
-    color: lightblue;
-  }
-  a:hover {
-    color: red;
-  }
-  a:active {
-    color: green;
-  }
-`;
-
-const Search = styled.button`
-  height: 40px;
-  width: 80px;
-  background-color: #4f4587;
-  border-radius: 30px;
-  border: 0;
-  outline: 0;
-  margin: 3px;
-  a:link {
-    color: pink;
-  }
-  a:visited {
-    color: lightblue;
-  }
-  a:hover {
-    color: red;
-  }
-  a:active {
-    color: green;
-  }
-`;
-
-const Parking = styled.button`
-  height: 40px;
-  width: 80px;
-  background-color: #4f4587;
-  border-radius: 30px;
-  border: 0;
-  outline: 0;
-  margin: 3px;
-  a:link {
-    color: pink;
-  }
-  a:visited {
-    color: lightblue;
-  }
-  a:hover {
-    color: red;
-  }
-  a:active {
-    color: green;
-  }
-`;
-
 export default function MenuBar() {
-    const keyword = useSelector((state) => state.keyword.value);
+  const [value, setValue] = React.useState(1);
+  const keyword = useSelector((state) => state.keyword.value);
 
-    function warning() {
-        if (keyword === '') {
-        alert('선택된 주차장이 없습니다.')
-        }
+  function warning() {
+    // 선택된 주차장이 없다면 search page로 이동
+    if (keyword === '') {
+      setValue(0)   // 메뉴바 searchpage 활성화
+      alert('선택된 주차장이 없습니다.')
     }
+  }
+  console.log(window.location.pathname);
+  useEffect(() => {
+  }, [])
 
   return (
     <Menubar>
-      <Search>
-        <Link to={`/SearchPage`}>Search</Link>
-      </Search>
-      <Home>
-        <Link to={`/`}>Home</Link>
-      </Home>
-      <Parking onClick={warning}>
-        <Link to={keyword ? `/${keyword.PARKING}` : `/SearchPage`}>
-          Parking
-        </Link>
-      </Parking>
+    <Box sx={{ width: 500 }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Search" icon={<SearchIcon />} component={Link} to={`/SearchPage`} />
+        <BottomNavigationAction label="Home" icon={<HomeIcon />} component={Link} to={`/`}/>
+        <BottomNavigationAction onClick={warning} label="Parking" icon={<LocalParkingIcon />}  
+        component={Link} to={keyword ? `/${keyword.PARKING}` : `/SearchPage`}/>
+      </BottomNavigation>
+    </Box>
     </Menubar>
   );
 }
