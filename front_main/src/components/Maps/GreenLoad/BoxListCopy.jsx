59460b9836b159ba1862f86d5653e1a0abf2a237
장우주 <ozoo109@naver.com>
2022-08-12 10:50:28 +0900

    boxlistcopy 임의생성

--- front_main/src/components/Maps/GreenLoad/BoxListCopy.jsx
+++ front_main/src/components/Maps/GreenLoad/BoxListCopy.jsx
@@ -1,71 +1,71 @@
-import styled from 'styled-components';
-import Box from '../Box'; // props를 위한 Box import
+// import styled from 'styled-components';
+// import Box from '../Box'; // props를 위한 Box import
 
-const ParkingSatus = styled.h2`
-  position: fixed;
-  top : 0;
-  left : 0;
-  right : 0;
-  align-items: center;
-  font-size : 40px;
-  background-color: #f7ccf7;
-  color: #222222;
-  text-shadow: 2px 2px 3px rgba(255,255,255,0.2);
+// const ParkingSatus = styled.h2`
+//   position: fixed;
+//   top : 0;
+//   left : 0;
+//   right : 0;
+//   align-items: center;
+//   font-size : 40px;
+//   background-color: #f7ccf7;
+//   color: #222222;
+//   text-shadow: 2px 2px 3px rgba(255,255,255,0.2);
 
-`
-const AllParking = styled.div`
-  flex-wrap : nowrap;
-  @media screen and (max-width: 700px) {
-        background-color: black;
-    }
-`
+// `
+// const AllParking = styled.div`
+//   flex-wrap : nowrap;
+//   @media screen and (max-width: 700px) {
+//         background-color: black;
+//     }
+// `
 
-const BackParking = styled.div`
-  width : 100%;
-  height: 100%;
-  margin-top : 200px;
-  margin-left : 200px;
-  margin-right : auto;
-  margin-bottom : 100px;
-  left : 200px;
-  display: flex;
+// const BackParking = styled.div`
+//   width : 100%;
+//   height: 100%;
+//   margin-top : 200px;
+//   margin-left : 200px;
+//   margin-right : auto;
+//   margin-bottom : 100px;
+//   left : 200px;
+//   display: flex;
   
-`
+// `
 
-const BrTask = styled.div`
-  margin-top : 300rem;
-  margin-bottom: 300rem;
-  `
+// const BrTask = styled.div`
+//   margin-top : 300rem;
+//   margin-bottom: 300rem;
+//   `
 
-const BoxList = ({ParkingData}) => {
-  console.log(ParkingData)
-  const TestData = ParkingData?.LIST
-  if (ParkingData) {return(
-    <>
-      <div>
-        <ParkingSatus>
-          {ParkingData.NAME} <br />
-          빈 : {ParkingData?.ENABLE} / 총 : {ParkingData?.TOTAL}
-          </ParkingSatus>
-          <AllParking>
-            <BackParking>
-                {
-                  TestData.filter(TestData => TestData.ID <= 7).map(TestData => (
-                    <Box key={ TestData.ID } TestData={ TestData } />
-                  ))
-                }
-            </BackParking>
-            <BrTask></BrTask>
-            <BackParking>
-                {
-                  TestData.filter(TestData => TestData.ID >= 8).map(TestData => (
-                    <Box key={ TestData.ID } TestData={ TestData } />
-                  ))
-                }
-            </BackParking>
-          </AllParking>
-      </div>
-    </>
-  )}
-}
-export default BoxList
+// const BoxList = ({ParkingData}) => {
+//   console.log(ParkingData)
+//   const TestData = ParkingData?.LIST
+//   if (ParkingData) {return(
+//     <>
+//       <div>
+//         <ParkingSatus>
+//           {ParkingData.NAME} <br />
+//           빈 : {ParkingData?.ENABLE} / 총 : {ParkingData?.TOTAL}
+//           </ParkingSatus>
+//           <AllParking>
+//             <BackParking>
+//                 {
+//                   TestData.filter(TestData => TestData.ID <= 7).map(TestData => (
+//                     <Box key={ TestData.ID } TestData={ TestData } />
+//                   ))
+//                 }
+//             </BackParking>
+//             <BrTask></BrTask>
+//             <BackParking>
+//                 {
+//                   TestData.filter(TestData => TestData.ID >= 8).map(TestData => (
+//                     <Box key={ TestData.ID } TestData={ TestData } />
+//                   ))
+//                 }
+//             </BackParking>
+//           </AllParking>
+//       </div>
+//     </>
+//   )}
+// }
+// export default BoxList
