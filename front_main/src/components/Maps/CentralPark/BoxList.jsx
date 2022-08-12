7b35ca06b2466106cdae8bc30b94ed07d23912d0
이성조 <lsj2837@naver.com>
2022-08-12 21:11:15 +0900

    병합중

--- front_main/src/components/Maps/CentralPark/BoxList.jsx
+++ front_main/src/components/Maps/CentralPark/BoxList.jsx
@@ -1,9 +1,5 @@
 import styled from "styled-components";
 import Box from "../Box"; // props를 위한 Box import
-// import {
-//   TransformComponent,
-//   TransformWrapper,
-// } from "@pronestor/react-zoom-pan-pinch";
 
 const ParkingSatus = styled.div`
   position: fixed;
@@ -38,12 +34,6 @@ const Info = styled.div`
 `;
 
 const AllParking = styled.div`
-<<<<<<< HEAD
-  /* flex-wrap: wrap; */
-  /* align-content: space-between; */
-  height: 100vh;
-  width: 100vw;
-=======
   flex-wrap: nowrap;
   right: 0px;
   flex: 1;
@@ -53,67 +43,29 @@ const BackParking = styled.div`
   width: 100%;
   height: 100%;
 
->>>>>>> 2a6ff0ea445758247d0222a554601b73fffe6480
   display: flex;
-  margin-left: 0;
-  margin-right: 0;
-  /* height: 100vh;
-  width: 100vw;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  align-content: flex-start; */
 `;
 
 const RotateBox = styled.div`
   display: flex;
   transform: rotate(90deg);
-<<<<<<< HEAD
-  margin-top: 20px;
-  margin-left: auto;
-  margin-right: auto;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  /* width: 300px;
-  height: 50px; */
-  /* left: 0px;
-  justify-content: space-between;
-  margin-top: 350px;
-  margin-left: 0px;
-  margin-right: auto; */
-`;
-const RightBox = styled.div`
-  display: flex;
-  align-items: flex-end;
-  /* margin-bottom: 150px; */
-  margin-top: 20px;
-  margin-left: auto;
-  margin-right: auto;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  /* justify-content: space-between;
-=======
   justify-content: space-between;
   margin-top: 300px;
   margin-left: 10%;
   margin-right: auto;
-  @media screen and (max-width:700px) {
+  @media screen and (max-width: 700px) {
     margin-left: -20%;
-    
   }
 `;
 const RightBox = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
->>>>>>> 2a6ff0ea445758247d0222a554601b73fffe6480
   margin-top: 150px;
   margin-left: 50%;
   transform: translate(-50%, 0);
   margin-right: 0px;
-  margin-bottom: 15px; */
+  margin-bottom: 150px;
 `;
 
 const BoxList = ({ ParkingData }) => {
@@ -128,19 +80,27 @@ const BoxList = ({ ParkingData }) => {
               주차 현황 : {ParkingData?.ENABLE} / {ParkingData?.TOTAL}
             </Info>
           </ParkingSatus>
+          <AllParking>
+            <BackParking>
+              <RotateBox>
+                {TestData.filter((TestData) => TestData.ID <= 4).map(
+                  (TestData) => (
+                    <Box key={TestData.ID} TestData={TestData} />
+                  )
+                )}
+              </RotateBox>
+            </BackParking>
+            <BackParking>
+              <RightBox>
+                {TestData.filter((TestData) => TestData.ID >= 5).map(
+                  (TestData) => (
+                    <Box key={TestData.ID} TestData={TestData} />
+                  )
+                )}
+              </RightBox>
+            </BackParking>
+          </AllParking>
         </div>
-        <AllParking>
-          <RotateBox>
-            {TestData.filter((TestData) => TestData.ID <= 4).map((TestData) => (
-              <Box key={TestData.ID} TestData={TestData} />
-            ))}
-          </RotateBox>
-          <RightBox>
-            {TestData.filter((TestData) => TestData.ID >= 5).map((TestData) => (
-              <Box key={TestData.ID} TestData={TestData} />
-            ))}
-          </RightBox>
-        </AllParking>
       </>
     );
   }
