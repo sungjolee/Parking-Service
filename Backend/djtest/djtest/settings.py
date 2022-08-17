5a1158068821eed5d9bc06232c6ace82d02e6302
jyp <bit6449@naver.com>
2022-08-17 16:29:13 +0900

    requirement edit

--- Backend/djtest/djtest/settings.py
+++ Backend/djtest/djtest/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import pymysql  
-pymysql.install_as_MySQLdb()
+# import pymysql  
+# pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'parkingAdmin',
-    'bootstrap4',
+    # 'bootstrap4',
     'sslserver'
 ]
 
