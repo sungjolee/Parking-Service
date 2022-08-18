df33a851fd246b86518081442f8381b96e509658
장우주 <ozoo109@naver.com>
2022-08-18 16:04:37 +0900

    fetch 지움

--- front_main/src/components/Search/SearchPage.jsx
+++ front_main/src/components/Search/SearchPage.jsx
@@ -24,10 +24,7 @@ function SearchPage() {
   const [datas, setDatas] = useState([]); // useState를 통한 값 저장
 
   useEffect(() => {
-    console.log(BASE_URL)
-    fetch(BASE_URL).then(res => console.log(res))
     axios.get(BASE_URL).then((response) => {
-      console.log(response)
       setDatas(response.data);
     });
   }, []);
