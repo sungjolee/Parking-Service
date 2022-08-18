989e6cdd508a0a0bb8af93bc227e3c2ab0f58afe
장우주 <ozoo109@naver.com>
2022-08-18 15:07:59 +0900

    https -> http 변경

--- front_main/src/components/Maps/GreenLoad/API.jsx
+++ front_main/src/components/Maps/GreenLoad/API.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import BoxList from "./BoxList"; // props를 위한 BoxList import
 
-const BASE_URL = "https://i7c103.p.ssafy.io:8000/parking/"; // 기본 url
+const BASE_URL = "http://i7c103.p.ssafy.io:8000/parking/"; // 기본 url
 
 const ParkingData = () => {
   const [ParkingData, setParkingData] = useState(); // useState를 통한 datas 값 저장
